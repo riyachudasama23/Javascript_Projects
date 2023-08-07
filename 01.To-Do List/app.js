78d27047ceac4ff6ad8ff5e66ac7201d3e3a0cf0
Riya <riyachudasama1123@gmail.com>
2023-08-07 21:58:38 +0530

    Final Commit

--- 01.To-Do List/app.js
+++ 01.To-Do List/app.js
@@ -1,2 +1,40 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+function addTask(){
+  if(inputBox.value === ''){
+    alert("You must write something!!");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;  //text inside li variable is value of inputBox
+    listContainer.appendChild(li);  //li value will be displayed in listContainer
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  savaData();
+}
+
+//for deleting the to-do task
+
+listContainer.addEventListener("click",function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+  }
+}, false);
+
+function savaData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
